a3ef08950ed370fa26b2bec98d62ae5dd5c86554
Niloy <shafkatniloy119@gmail.com>
2026-07-23 03:48:35 +0600

    add global validation

--- Program.cs
+++ Program.cs
@@ -5,17 +5,17 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
-builder.Services.AddControllers()
+builder.Services.AddControllers();
 
-.ConfigureApiBehaviorOptions(Options =>
+builder.Services.Configure<ApiBehaviorOptions>(Options =>
 {
     Options.InvalidModelStateResponseFactory = context =>
     {
-        var errors = context.ModelState.Where(e => e.Value.Errors.Count > 0)
+        var errors = context.ModelState.Where(e => e.Value !=null && e.Value.Errors.Count > 0)
                 .Select(e => new
                 {
                     Field = e.Key,
-                    Errors = e.Value.Errors.Select(x => x.ErrorMessage).ToArray()
+                    Errors = e.Value!=null ? e.Value.Errors.Select(x => x.ErrorMessage).ToArray() : new string[0]
 
                 }).ToList();
 
